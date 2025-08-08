bcfaca1cd34a56f9d10c96e57a6713a4dd8a4d5d
Mathias Custódio Moreira <mathias.moreira@brgaap.com>
2025-08-08 17:32:41 -0300

    Adding client services

---
+++ HR.LeaveManagement.BlazorUI/Services/LeaveTypeService.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Models.LeaveTypes;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+
+namespace HR.LeaveManagement.BlazorUI.Services;
+
+public class LeaveTypeService(IClient client, IMapper mapper) : BaseHttpService(client), ILeaveTypeService
+{
+    public async Task<List<LeaveTypeVM>> GetLeaveTypes()
+    {
+        var leaveTypes = await _client.LeaveTypesAllAsync();
+        return mapper.Map<List<LeaveTypeVM>>(leaveTypes);
+    }
+
+    public async Task<LeaveTypeVM> GetLeaveTypeDetails(int id)
+    {
+        var leaveType = await _client.LeaveTypesGETAsync(id);
+        return mapper.Map<LeaveTypeVM>(leaveType);
+    }
+
+    public async Task<Response<Guid>> CreateLeaveType(LeaveTypeVM leaveType)
+    {
+        try
+        {
+            var createLeaveTypeCommand = mapper.Map<CreateLeaveTypeCommand>(leaveType);
+            await _client.LeaveTypesPOSTAsync(createLeaveTypeCommand);
+            return new Response<Guid>()
+            {
+                Success = true,
+            };
+        }
+        catch (ApiException ex)
+        {
+
+            return ConvertApiExceptions<Guid>(ex);
+        }
+    }
+
+    public async Task<Response<Guid>> DeleteLeaveType(int id)
+    {
+        try
+        {
+            await _client.LeaveTypesDELETEAsync(id);
+            return new Response<Guid>() { Success = true };
+        }
+        catch (ApiException ex)
+        {
+            return ConvertApiExceptions<Guid>(ex);
+        }
+    }   
+
+    public async Task<Response<Guid>> UpdateLeaveType(int id, LeaveTypeVM leaveType)
+    {
+        try
+        {
+            var updateLeaveTypeCommand = mapper.Map<UpdateLeaveTypeCommand>(leaveType);
+            await _client.LeaveTypesPUTAsync(id.ToString(), updateLeaveTypeCommand);
+            return new Response<Guid>()
+            {
+                Success = true,
+            };
+        }
+        catch (ApiException ex)
+        {
+            return ConvertApiExceptions<Guid>(ex);
+        }
+    }
+}
